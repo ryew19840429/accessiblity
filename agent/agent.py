314f8f7c31f91f1ca89b8d03fec3618310b975be
raymond yong <ryew.1984@gmail.com>
2025-10-31 16:34:14 +0100

    kinda works

--- agent/agent.py
+++ agent/agent.py
@@ -12,6 +12,8 @@ from ag_ui_adk import ADKAgent, add_adk_fastapi_endpoint
 
 # ADK imports
 from google.adk.agents import LlmAgent
+from google.adk.agents.llm_agent import Agent
+from google.adk.tools.agent_tool import AgentTool
 from google.adk.agents.callback_context import CallbackContext
 from google.adk.sessions import InMemorySessionService, Session
 from google.adk.runners import Runner
@@ -20,6 +22,8 @@ from google.adk.tools import FunctionTool, ToolContext
 from google.genai.types import Content, Part , FunctionDeclaration
 from google.adk.models import LlmResponse, LlmRequest
 from google.genai import types
+from tools import get_gvb_disruptions, get_public_transport_route, get_weather_forecast
+from google.adk.tools import google_search
 
 from pydantic import BaseModel, Field
 from typing import List, Optional
@@ -183,10 +187,27 @@ proverbs_agent = LlmAgent(
         after_model_callback = simple_after_model_modifier
     )
 
+Agent_Search = Agent(
+    model='gemini-2.0-flash-exp',
+    name='SearchAgent',
+    instruction="""
+    You're a spealist in Google Search
+    """,
+    tools=[google_search]
+)
+
+root_agent = Agent(
+    model='gemini-2.5-flash',
+    name='root_agent',
+    description='A helpful assistant for user questions. When providing public transport routes, also check for and include any disruptions (e.g., lift or escalator outages) at the origin and destination stations, and provide weather recommendations for the destination.',
+    instruction='Answer user questions to the best of your knowledge. When providing public transport routes, also check for and include any disruptions (e.g., lift or escalator outages) at the origin and destination stations. When asked for weather, use the get_weather_forecast tool, ensuring to provide literal latitude, longitude, and a Unix timestamp for the desired time.',
+    tools=[get_gvb_disruptions, get_public_transport_route, get_weather_forecast,  AgentTool(agent=Agent_Search)]
+)
+
 # Create ADK middleware agent instance
 adk_proverbs_agent = ADKAgent(
-    adk_agent=proverbs_agent,
-    app_name="proverbs_app",
+    adk_agent=root_agent,
+    app_name="root_agent",
     user_id="demo_user",
     session_timeout_seconds=3600,
     use_in_memory_services=True
